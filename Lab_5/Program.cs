2e636b2f079374d405411f2cec2c6dbac478a1ce
CYKERO4KA <cykero4ka88@gmail.com>
2023-03-01 21:39:27 +0200

    First commit

---
+++ Lab_5/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Lab_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    abstract class Forest
+    {
+        public List<string> Trees;
+        public List<string> Bushes;
+    }
+
+    class Tree : Forest
+    {
+        public string Type;
+        public double Heigh;
+        public int Age;
+    }
+
+    class Bush : Forest
+    {
+        public bool IsBerries;
+    }
+
+    class Berry : Bush
+    {
+        
+    }
+    
+    class ChristmasTree : Tree
+    {
+        
+    }
+
+
+    class BlueBerries : Berry
+    {
+        
+    }
+
+    class Pine : Tree
+    {
+        
+    }
+
+    class Birch : Tree
+    {
+        
+    }
+    
+}
+
